41e94c16368cfd67f0aa5b8adcc7ee3f6eab254a
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2024-11-16 02:05:59 +0530

    image of tiet and some login modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Text } from "@mantine/core";
+import { px, Text } from "@mantine/core";
 import { AuthenticationForm } from "../components/Auth/Form";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../src/firebase";
@@ -11,23 +11,17 @@ function App() {
   });
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <img className="logo" src="images.png" width={175} height={150}>
+        
+      
+      </img>
+      <h1>Welcome to Pickupp</h1>
+      
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+     
       <AuthenticationForm auth={auth} db={db} />
 
-      <Text size="lg">Mantine Check</Text>
+      <Text size="lg">Made by PixelEngineers</Text>
     </main>
   );
 }
